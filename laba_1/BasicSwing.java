f4ae37cb0aa51cee942b325e42514d1f9ac86d22
Alexandra <alexandra.poznyak1@gmail.com>
2018-03-14 10:50:54 +0300

    finished lab

--- laba_1/BasicSwing.java
+++ laba_1/BasicSwing.java
@@ -1,57 +1,53 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
-import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
-public class BasicSwing extends JFrame{
+public class BasicSwing extends JFrame {
 
-    JPanel panel=new JPanel();
-    JButton button=new JButton("");
-    JTextField text=new JTextField("Text",10);
-    List<String> choices = new ArrayList<String>();
-    JComboBox<String> combobox=new JComboBox<>(choices);
+    JTextField textField;
+    JComboBox<String> comboBox;
+    JButton button;
 
-    public class ButtonActionListener implements ActionListener{
-        public void actionPerformed(ActionEvent e){
-            choices.add(text.getText());
-            text.setText("");
+    public BasicSwing() {
+        comboBox = new JComboBox<String>();
+        textField = new JTextField(" ", 20);
+        textField.setEditable(true);
+        button = new JButton("Push");
+        button.addActionListener(new action());
+        add(textField);
+        add(comboBox);
+        add(button);
 
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300,300);
+        setVisible(true);
+    }
+
+    public class action implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            boolean exists = false;
+            for (int index = 0; index < comboBox.getItemCount() && !exists; index++){
+                if (textField.getText().equals(comboBox.getItemAt(index))){
+                    JOptionPane.showMessageDialog(null,"alert", "alert", JOptionPane.ERROR_MESSAGE);
+                    exists = true;
+                }
+            }
+            if (!exists) {
+                comboBox.addItem(textField.getText());
+            }
 
         }
     }
 
-    public static void main(String[] args){
-        new BasicSwing();
-    }
-
-    public BasicSwing(){
-
-        super("Basic Swing App");
-
-        setSize(400,300);
-        setResizable(true);
-
-        panel.add(button);
-        panel.add(text);
-        panel.add(combobox);
-        add(panel);
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        setVisible(true);
-
-        ActionListener actionListener = new ButtonActionListener();
-        button.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                choices.add(text.getText());
-                text.setText("");
-
-            }
-        });
+    public static void main(String[] args) {
+        BasicSwing test = new BasicSwing();
     }
 }
+
